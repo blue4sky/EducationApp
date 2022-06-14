de578fe8b2f2292cde8d8794ce93321900aa4e02
blue4sky <sultanrifqi04@gmail.com>
2022-06-14 15:32:56 +0300

    Read Chat Data from Firestore

--- lib/view/main/discussion/chat_page.dart
+++ lib/view/main/discussion/chat_page.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_app/constant/r.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -14,13 +15,35 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   final textController = TextEditingController();
+
+  late CollectionReference chat;
+  late QuerySnapshot chatData;
+  // List<QueryDocumentSnapshot>? listChat;
+
+  // getDataFromFirebase() async {
+  //   chatData = await FirebaseFirestore.instance
+  //       .collection("room")
+  //       .doc("kimia")
+  //       .collection("chat")
+  //       .get();
+  //   // listChat = chatData.docs;
+  //   setState(() {});
+  // }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getDataFromFirebase();
+  }
+
   @override
   Widget build(BuildContext context) {
-    CollectionReference chat = FirebaseFirestore.instance
+    chat = FirebaseFirestore.instance
         .collection("room")
         .doc("kimia")
         .collection("chat");
-
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(
         title: Text("Diskusi Soal"),
@@ -29,48 +52,78 @@ class _ChatPageState extends State<ChatPage> {
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Nama User",
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Color(0xff5200FF),
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: chat.orderBy("time").snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
+
+                    return ListView.builder(
+                      // Reversed for flipping the message in the correct order
+                      itemCount: snapshot.data!.docs.reversed.length,
+                      // Display from bottom to top (like SocMed App)
+                      reverse: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        final currentChat = snapshot.data!.docs.reversed.toList()[index];
+                        final currentDate = (currentChat["time"] as Timestamp?)
+                            ?.toDate(); // Date can be null
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: Column(
+                            crossAxisAlignment: user.uid == currentChat["uid"]
+                                ? CrossAxisAlignment.end
+                                : CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                currentChat["nama"],
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color(0xff5200FF),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 4),
+                                decoration: BoxDecoration(
+                                  color: user.uid == currentChat["uid"]
+                                      ? Colors.green.withOpacity(0.5)
+                                      : Color(0xffffdcdc),
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: user.uid == currentChat["uid"]
+                                        ? Radius.circular(0)
+                                        : Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                    topLeft: user.uid != currentChat["uid"]
+                                        ? Radius.circular(0)
+                                        : Radius.circular(10),
+                                  ),
+                                ),
+                                child: Text(
+                                  currentChat["content"],
+                                ),
+                              ),
+                              Text(
+                                currentDate == null
+                                    ? ""
+                                    : DateFormat("dd-MMM-yyy HH:mm")
+                                        .format(currentDate),
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: R.colours.greySubtitleHome,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                        Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-                          decoration: BoxDecoration(
-                            color: Color(0xffffdcdc),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
-                              topRight: Radius.circular(10),
-                            ),
-                          ),
-                          child: Text("Pesan"),
-                        ),
-                        Text(
-                          "Waktu Kirim",
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: R.colours.greySubtitleHome,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
+                        );
+                      },
+                    );
+                  },
+                )),
           ),
           SafeArea(
             child: Container(
@@ -131,8 +184,6 @@ class _ChatPageState extends State<ChatPage> {
                         return;
                       }
 
-                      final user = FirebaseAuth.instance.currentUser!;
-
                       final chatContent = {
                         "nama": user.displayName,
                         "uid": user.uid,
@@ -141,7 +192,9 @@ class _ChatPageState extends State<ChatPage> {
                         "photo": user.photoURL,
                         "time": FieldValue.serverTimestamp(),
                       };
-                      chat.add(chatContent);
+                      chat.add(chatContent).whenComplete(() {
+                        textController.clear();
+                      });
                     },
                     icon: Icon(
                       Icons.send,
