cfdf7f40652fe4a2d6b4d357b387625df9d10c24
blue4sky <sultanrifqi04@gmail.com>
2022-08-11 19:56:21 +0700

    Profile page scrollable

--- lib/view/main/profile/profile_page.dart
+++ lib/view/main/profile/profile_page.dart
@@ -60,189 +60,191 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       body: user == null
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(
-                    top: 28,
-                    bottom: 35,
-                    right: 15,
-                    left: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    color: R.colours.primary,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(9),
-                      bottomRight: Radius.circular(9),
+          : SingleChildScrollView(
+            child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 28,
+                      bottom: 35,
+                      right: 15,
+                      left: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: R.colours.primary,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(9),
+                        bottomRight: Radius.circular(9),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                user!.userName!,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                user!.userAsalSekolah!,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // Profile Picture
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(user!.userFoto!),
+                          radius: 40,
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              user!.userName!,
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Text(
-                              user!.userAsalSekolah!,
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      // Profile Picture
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(user!.userFoto!),
-                        radius: 40,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 15),
-                Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 7, color: Colors.black.withOpacity(0.25))
-                    ],
-                  ),
-                  margin: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
-                  padding: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Profile",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        "Full Name",
-                        style: TextStyle(
-                          color: R.colours.greySubtitleHome,
-                          fontSize: 12,
-                        ),
-                      ),
-                      Text(
-                        user!.userName!,
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        "Email",
-                        style: TextStyle(
-                          color: R.colours.greySubtitleHome,
-                          fontSize: 12,
-                        ),
-                      ),
-                      Text(
-                        user!.userEmail!,
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        "Gender",
-                        style: TextStyle(
-                          color: R.colours.greySubtitleHome,
-                          fontSize: 12,
-                        ),
-                      ),
-                      Text(
-                        user!.userGender!,
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        "Class",
-                        style: TextStyle(
-                          color: R.colours.greySubtitleHome,
-                          fontSize: 12,
-                        ),
-                      ),
-                      Text(
-                        user!.kelas!,
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        "School",
-                        style: TextStyle(
-                          color: R.colours.greySubtitleHome,
-                          fontSize: 12,
-                        ),
-                      ),
-                      Text(
-                        user!.userAsalSekolah!,
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (() async {
-                    await GoogleSignIn().signOut();
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                        LoginPage.route, (route) => false);
-                  }),
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 13),
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  SizedBox(height: 15),
+                  Container(
+                    width: double.infinity,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
-                          blurRadius: 7,
-                          color: Colors.black.withOpacity(0.25),
-                        ),
+                            blurRadius: 7, color: Colors.black.withOpacity(0.25))
                       ],
                     ),
-                    child: Row(
+                    margin: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Icon(
-                          Icons.exit_to_app,
-                          color: Colors.red,
-                        ),
-                        SizedBox(width: 10),
                         Text(
-                          "Log Out",
+                          "Profile",
                           style: TextStyle(
-                            color: Colors.red,
-                            // fontSize: 12,
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Full Name",
+                          style: TextStyle(
+                            color: R.colours.greySubtitleHome,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          user!.userName!,
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                            color: R.colours.greySubtitleHome,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          user!.userEmail!,
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Gender",
+                          style: TextStyle(
+                            color: R.colours.greySubtitleHome,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          user!.userGender!,
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Class",
+                          style: TextStyle(
+                            color: R.colours.greySubtitleHome,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          user!.kelas!,
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "School",
+                          style: TextStyle(
+                            color: R.colours.greySubtitleHome,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          user!.userAsalSekolah!,
+                          style: TextStyle(
+                            fontSize: 13,
                           ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
+                  GestureDetector(
+                    onTap: (() async {
+                      await GoogleSignIn().signOut();
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                          LoginPage.route, (route) => false);
+                    }),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 13),
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 7,
+                            color: Colors.black.withOpacity(0.25),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.exit_to_app,
+                            color: Colors.red,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Log Out",
+                            style: TextStyle(
+                              color: Colors.red,
+                              // fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ),
     );
   }
 }
